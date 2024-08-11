93f9f75b1aa6a1ba54ccaab3a52f7a6b5256ae36
Leon Watermann <leon.github@leonsdepot.de>
2024-08-11 20:14:20 +0200

    Add ability to highlight page search results

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,16 @@
+let keyHookEnabled = true;
 document.addEventListener( 'keydown', ( event ) => {
-  if ( ( event.ctrlKey || event.metaKey ) && event.key === 'f' ) {
+  if ( ( event.ctrlKey || event.metaKey ) && event.key === 'f' && keyHookEnabled ) {
+    keyHookEnabled = false;
+    
     const highlighter = new Highlighter( browser.runtime.getURL( 'img/ring.svg' ) );
-    console.log( 'FineFind Test' );
+
+    document.addEventListener( 'selectionchange', () => {
+      const select = window.getSelection();
+      const pos = select.getRangeAt( 0 ).getClientRects()[0];
+
+      highlighter.moveTo( ( pos.left + ( pos.width / 2 ) ), ( pos.top + ( pos.height / 2 ) ) );
+      highlighter.animate();
+    })
   }
 })
