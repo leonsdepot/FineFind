fe6162ba9bf3eb3b5a1f517ff67d87a85af8e35c
Leon Watermann <leon.github@leonsdepot.de>
2025-01-06 13:00:21 +0100

    Fix broken Ctrl+F detection

--- src/js/content-script.js
+++ src/js/content-script.js
@@ -32,6 +32,14 @@ document.addEventListener( 'focus', () => {
   isUserSelect = true;
 })
 
+document.addEventListener( 'mousedown', () => {
+  isUserSelect = true;
+})
+
+document.addEventListener( 'keydown', () => {
+  isUserSelect = true;
+})
+
 document.addEventListener( 'blur', () => {
   isUserSelect = false;
 })
